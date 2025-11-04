577277a78e6f5d1aa188c0925e47a8f06158293f
Ilya Kolomoytsev <i.kolomoitsev@yandex.ru>
2025-11-04 12:48:47 +0300

    ha: introduce ha signal type enum

---
+++ src/ha/model/ha-phy-common.h
@@ -0,0 +1,14 @@
+#ifndef NS3_HA_PHY_COMMON_H
+#define NS3_HA_PHY_COMMON_H
+
+namespace ns3
+{
+
+enum HaSignalType
+{
+    HA_SIGNAL_UNKNOWN = 0,
+};
+
+} // namespace ns3
+
+#endif // NS3_HA_PHY_COMMON_H
